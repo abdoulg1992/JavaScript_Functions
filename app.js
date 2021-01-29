0c4377802950c162456467dc18f9b8e2971668fc
Abdoul Dgit config --global user.email abdoulson02@gmail.com <abdoulson02@gmail.com>
2021-01-29 10:58:34 -0500

    functions application

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-console.log("Hello World!\n==========\n");
+//console.log("Hello World!\n==========\n");
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+/*--- // Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");--*/
 
+function printOdds(count) {
+    for ( let i = 1; i <= 100; i++ ) {
+        if ( i % 2 == 1 ) { 
+            console.log(i);
+    }
+}
+
+} printOdds(500);
+    
+
+
+
+
+
+/*
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 2:\n==========\n");--*/
